367953bceb9b2526c86973fa6608f6e5b84be2b7
Patrick Her <renfu.her@gmail.com>
2024-11-21 17:46:02 +0800

    Add logout route to app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin/upload-image',
             'checkout/map/rewrite',
             'payment/*',
+            'logout',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
