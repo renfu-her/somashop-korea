05fccc169a890e5eb952f6cf80bfdd498eb2bf81
Patrick Her <renfu.her@gmail.com>
2024-11-21 09:18:59 +0800

    Refactor PaymentController to include payment callback functionality

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -193,6 +193,8 @@ class PaymentController extends Controller
     // 付款結果
     public function paymentCallback(Request $request)
     {
+
+        dd($request->all());
         $paymentResult = $request->all();
 
         // 根據訂單編號查詢訂單
