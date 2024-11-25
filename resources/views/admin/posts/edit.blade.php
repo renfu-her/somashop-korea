5d46ba17e4e31af7dd7fa1fc7a0f7b1c08cd3b1c
Patrick Her <renfu.her@gmail.com>
2024-11-25 12:56:15 +0800

    🎨 View: 更新 package-lock.json 等 10 個檔案

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -68,119 +68,160 @@
 @endpush
 
 @push('scripts')
-    <script src="{{ asset('ckeditor5/ckeditor.js') }}"></script>
     <script src="{{ asset('ckeditor5/zh.min.js') }}"></script>
+    <script type="importmap">
+        {
+            "imports": {
+                "ckeditor5": "https://cdn.ckeditor.com/ckeditor5/43.3.1/ckeditor5.js",
+                "ckeditor5/": "https://cdn.ckeditor.com/ckeditor5/43.3.1/"
+            }
+        }
+    </script>
+    <script type="module">
+        import {
+            ClassicEditor,
+            Essentials,
+            Paragraph,
+            Bold,
+            Italic,
+            Font,
+            Alignment,
+            Heading,
+            List,
+            Link,
+            Image,
+            Table,
+            MediaEmbed,
+            ImageUpload
+        } from 'ckeditor5';
+
+        ClassicEditor
+            .create(document.querySelector('#content'), {
+                language: 'zh',
+                ckfinder: {
+                    uploadUrl: '{{ route('admin.upload.image') }}',
+                    upload: {
+                        types: ['jpeg', 'png', 'gif', 'jpg', 'webp']
+                    }
+                },
+                plugins: [
+                    Essentials,
+                    Paragraph,
+                    Bold,
+                    Italic,
+                    Font,
+                    Alignment,
+                    Heading,
+                    List,
+                    Link,
+                    Image,
+                    Table,
+                    MediaEmbed,
+                    ImageUpload
+                ],
+                toolbar: {
+                    items: [
+                        'heading', '|',
+                        'fontSize', 'fontFamily', '|',
+                        'fontColor', 'fontBackgroundColor', '|',
+                        'bold', 'italic', 'underline', 'strikethrough', '|',
+                        'alignment',
+                        '|',
+                        'bulletedList', 'numberedList', '|',
+                        'outdent', 'indent', '|',
+                        'link', 'imageUpload', 'mediaEmbed', '|',
+                        'blockQuote', 'insertTable', '|',
+                        'undo', 'redo'
+                    ]
+                },
+                alignment: {
+                    options: ['left', 'center', 'right', 'justify']
+                },
+                heading: {
+                    options: [{
+                            model: 'paragraph',
+                            title: '段落',
+                            class: 'ck-heading_paragraph'
+                        },
+                        {
+                            model: 'heading1',
+                            view: 'h1',
+                            title: '標題 1',
+                            class: 'ck-heading_heading1'
+                        },
+                        {
+                            model: 'heading2',
+                            view: 'h2',
+                            title: '標題 2',
+                            class: 'ck-heading_heading2'
+                        },
+                        {
+                            model: 'heading3',
+                            view: 'h3',
+                            title: '標題 3',
+                            class: 'ck-heading_heading3'
+                        }
+                    ]
+                },
+                fontSize: {
+                    options: [
+                        'tiny',
+                        'small',
+                        'default',
+                        'big',
+                        'huge'
+                    ]
+                },
+                fontFamily: {
+                    options: [
+                        'default',
+                        '微軟正黑體',
+                        '新細明體',
+                        '標楷體',
+                        'Arial',
+                        'Times New Roman'
+                    ]
+                },
+                image: {
+                    toolbar: [
+                        'imageTextAlternative', '|',
+                        'imageStyle:alignLeft',
+                        'imageStyle:alignCenter',
+                        'imageStyle:alignRight', '|',
+                        'imageStyle:full',
+                        'imageStyle:side'
+                    ],
+                    styles: [
+                        'full',
+                        'side',
+                        'alignLeft',
+                        'alignCenter',
+                        'alignRight'
+                    ]
+                },
+                table: {
+                    contentToolbar: [
+                        'tableColumn',
+                        'tableRow',
+                        'mergeTableCells',
+                        'tableCellProperties',
+                        'tableProperties'
+                    ]
+                },
+                mediaEmbed: {
+                    previewsInData: true
+                }
+            })
+            .then(editor => {
+                window.editor = editor;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
+
     <script>
         $(document).ready(function() {
-            let editor;
-            ClassicEditor
-                .create(document.querySelector('#content'), {
-                    language: 'zh',
-                    ckfinder: {
-                        uploadUrl: '{{ route('admin.upload.image') }}',
-                        upload: {
-                            types: ['jpeg', 'png', 'gif', 'jpg', 'webp']
-                        }
-                    },
-                    toolbar: {
-                        items: [
-                            'heading', '|',
-                            'fontSize', 'fontFamily', '|',
-                            'fontColor', 'fontBackgroundColor', '|',
-                            'bold', 'italic', 'underline', 'strikethrough', '|',
-                            'alignment:left', 'alignment:center', 'alignment:right', 'alignment:justify',
-                            '|',
-                            'bulletedList', 'numberedList', '|',
-                            'outdent', 'indent', '|',
-                            'link', 'imageUpload', 'mediaEmbed', '|',
-                            'blockQuote', 'insertTable', '|',
-                            'undo', 'redo'
-                        ]
-                    },
-                    alignment: {
-                        options: ['left', 'center', 'right', 'justify']
-                    },
-                    heading: {
-                        options: [{
-                                model: 'paragraph',
-                                title: '段落',
-                                class: 'ck-heading_paragraph'
-                            },
-                            {
-                                model: 'heading1',
-                                view: 'h1',
-                                title: '標題 1',
-                                class: 'ck-heading_heading1'
-                            },
-                            {
-                                model: 'heading2',
-                                view: 'h2',
-                                title: '標題 2',
-                                class: 'ck-heading_heading2'
-                            },
-                            {
-                                model: 'heading3',
-                                view: 'h3',
-                                title: '標題 3',
-                                class: 'ck-heading_heading3'
-                            }
-                        ]
-                    },
-                    fontSize: {
-                        options: [
-                            'tiny',
-                            'small',
-                            'default',
-                            'big',
-                            'huge'
-                        ]
-                    },
-                    fontFamily: {
-                        options: [
-                            'default',
-                            '微軟正黑體',
-                            '新細明體',
-                            '標楷體',
-                            'Arial',
-                            'Times New Roman'
-                        ]
-                    },
-                    image: {
-                        toolbar: [
-                            'imageTextAlternative', '|',
-                            'imageStyle:alignLeft',
-                            'imageStyle:alignCenter',
-                            'imageStyle:alignRight', '|',
-                            'imageStyle:full',
-                            'imageStyle:side'
-                        ],
-                        styles: [
-                            'full',
-                            'side',
-                            'alignLeft',
-                            'alignCenter',
-                            'alignRight'
-                        ]
-                    },
-                    table: {
-                        contentToolbar: [
-                            'tableColumn',
-                            'tableRow',
-                            'mergeTableCells',
-                            'tableCellProperties',
-                            'tableProperties'
-                        ]
-                    },
-                    mediaEmbed: {
-                        previewsInData: true
-                    }
-                })
-                .then(newEditor => {
-                    editor = newEditor;
-                })
-                .catch(error => {
-                    console.error('編輯器初始化失敗', error);
-                });
 
             $('form').on('submit', function(e) {
                 const content = editor.getData();
