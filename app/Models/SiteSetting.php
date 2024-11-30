78ee363d179bc86b39aa4002fa11f0167be67ea5
Patrick Her <renfu.her@gmail.com>
2024-11-30 11:16:49 +0800

    📊 Model: 更新 Cart.php 等 6 個檔案

--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class SiteSetting extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'site_name',
         'site_description',
