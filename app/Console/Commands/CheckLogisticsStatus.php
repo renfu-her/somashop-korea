7189e42bf561c2ef1690c75e33324edbde32d2ce
Patrick Her <renfu.her@gmail.com>
2025-04-09 16:43:52 +0800

    在 CheckLogisticsStatus 指令中新增調試輸出，顯示 hashKey 和 hashIV 的值，以便於檢查生成的檢查碼值。

--- app/Console/Commands/CheckLogisticsStatus.php
+++ app/Console/Commands/CheckLogisticsStatus.php
@@ -109,6 +109,8 @@ class CheckLogisticsStatus extends Command
         $hashKey = config('app.env') === 'production' ? config('config.ecpay_hash_key') : config('config.ecpay_stage_hash_key');
         $hashIV = config('app.env') === 'production' ? config('config.ecpay_hash_iv') : config('config.ecpay_stage_hash_iv');
 
+        dd($hashKey, $hashIV);
+
         // 1. 參數依照字母順序排序
         ksort($params);
 
