85684032867acb6552d898aa4c8c723f62e25b6b
Patrick Her <renfu.her@gmail.com>
2025-09-23 17:14:06 +0800

    更新後台管理系統的圖示，將網站圖標從 favicon.svg 更改為 logo.png，以提升品牌識別度。

--- resources/views/admin/layouts/auth.blade.php
+++ resources/views/admin/layouts/auth.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>後台管理系統 - @yield('title', '登入')</title>
 
-    <link rel="icon" href="{{ asset('frontend/img/favicon.svg') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('frontend/img/logo.png') }}" type="image/x-icon">
 
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
