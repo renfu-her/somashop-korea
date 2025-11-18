655e9623db42855e93f9436c7eb751336ea8cf8f
Patrick Her <renfu.her@gmail.com>
2025-11-18 16:05:58 +0800

    Add Instagram icon image to social link section in app layout for enhanced visual appeal.

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -110,6 +110,7 @@
                 <div
                     class="col-auto social-link d-flex justify-content-between align-items-center ml-auto text-center px-md-3 px-4">
                     <div class="px-md-3 px-0 hvr-bob">
+                        <img src="{{ asset('frontend/img/IG_icon.png') }}" class="img-fluid px-md-0 px-3" style="width: 100px; height: 100px;" />
                         <img src="{{ asset('frontend/img/talk_icon2.png') }}" class="img-fluid px-md-0 px-3" style="width: 100px; height: 100px;" />
                     </div>
                 </div>
